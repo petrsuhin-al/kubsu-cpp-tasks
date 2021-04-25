569fba6e430c1a37c4f15db1e56ae1c858eba4f1
al-petrushin <stuartrevol@gmail.com>
2021-04-26 01:31:52 +0300

    First Task: updates

--- task1/test/test.cpp
+++ task1/test/test.cpp
@@ -7,21 +7,25 @@
 
 using namespace std;
 
+void printMatrix(string title, vector<vector<double>> matrix) {
+    cout << title << endl;
+
+    for (int i = 0; i < matrix.size(); ++i) {
+        for (int j = 0; j < matrix[i].size(); j++) {
+            cout << matrix[i][j] << ' ';
+        }
+
+        cout << "\n";
+    }
+}
+
 void TestClassicMult() {
     vector<vector<double>> first = {{1, 2, 3}, {1, 2, 3}, {1, 2, 3}};
     vector<vector<double>> second = {{1, 2, 3}, {1, 2, 3}, {1, 2, 3}};
 
     auto solution = Classic(first, second);
 
-    cout << "Classic matrix multiplication: " << endl;
-
-    for (int i = 0; i < solution.size(); ++i) {
-        for (int j = 0; j < solution[i].size(); j++) {
-            cout << solution[i][j] << ' ';
-        }
-
-        cout << "\n";
-    }
+    printMatrix("Classic matrix multiplication:", solution);
 }
 
 int main() {
